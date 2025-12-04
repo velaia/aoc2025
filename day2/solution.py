63b47721aa989f8954d7ba07cd072cb70484b541
Daniel N. Lang <daniel.lang@velaia.de>
2025-12-04 15:21:41 +0100

    day 2 p 2

--- day2/solution.py
+++ day2/solution.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import re
 
 def is_invalid(id: int) -> bool:
     id = str(id)
@@ -6,7 +7,19 @@ def is_invalid(id: int) -> bool:
         return True
     return False
 
-def find_invalid_ids(file: Path) -> list[int]:
+def is_invalid2(id: int) -> bool:
+    if id == 2121212121:
+        ...
+    id_len = len(str(id))
+    for i in range(1, id_len//2 + 1):
+        p = re.compile("(" + str(id)[:i] + "){" + str(id_len // i) + "}")
+        if p.match(str(id)):
+            if i * (id_len // i) == id_len:
+                return True
+    return False
+
+
+def find_invalid_ids(file: Path, fn) -> list[int]:
     solution = []
 
     # get ranges
@@ -18,7 +31,7 @@ def find_invalid_ids(file: Path) -> list[int]:
     for ran in ranges:
         lower, upper = ran.split("-")
         for i in range(int(lower), int(upper) + 1):
-            if is_invalid(i):
+            if fn(i):
                 solution.append(i)
 
     return solution
@@ -29,10 +42,22 @@ if __name__ == "__main__":
     assert is_invalid(123123) is True
     assert is_invalid(123456) is False
 
-    solution = sum(find_invalid_ids(Path("day2/sample1.txt")))
+    solution = sum(find_invalid_ids(Path("day2/sample1.txt"), is_invalid))
 
     assert solution == 1227775554
 
-    solution2 = sum(find_invalid_ids(Path("day2/input1.txt")))
+    solution2 = sum(find_invalid_ids(Path("day2/input1.txt"), is_invalid))
     print(f"day 2 p 1: {solution2}")
+
+    # part 2
+    assert is_invalid2(121212) is True
+    assert is_invalid2(1212) is True
+    assert is_invalid2(12123) is False
+    assert is_invalid2(32323232323232) is True
+
+    invalid_ids = find_invalid_ids(Path("day2/sample1.txt"), is_invalid2)
+    solution4 = sum(invalid_ids)
+    assert 4174379265 == solution4
     
+    solution3 = sum(find_invalid_ids(Path("day2/input1.txt"), is_invalid2))
+    print(f"day 2 p 2: {solution3}")
